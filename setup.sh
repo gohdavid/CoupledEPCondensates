180e054c618c336a0a41e7eaed457ae866bcf1c2
davidgoh <davidgoh@node1192.inband>
2023-10-18 22:23:27 -0400

    Add symlinks to Conda bin

--- setup.sh
+++ setup.sh
@@ -10,3 +10,14 @@
 
 # Install utils package and dependecies from poetry.lock file
 # poetry install
+
+# Copy Gmsh compiled at engaging
+# module load gmsh/4.10.5
+# cp $(which gmsh) ${CONDA_PREFIX}/bin/
+
+# symlinks
+ln -s $(pwd)/utils/scripts/run_simulation.py ${CONDA_PREFIX}/bin/run-simulation
+ln -s $(pwd)/utils/scripts/sweep_parameters.py ${CONDA_PREFIX}/bin/sweep-parameter
+
+chmod +x  ${CONDA_PREFIX}/bin/run-simulation
+chmod +x  ${CONDA_PREFIX}/bin/sweep-parameter
