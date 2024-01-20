b507dffa4f52cdcd7c658c38480eed7485d3bce5
davidgoh <davidgoh@node1193.inband>
2024-01-20 15:03:16 -0500

    Override simulation slurm script

--- utils/scripts/sweep_parameters.py
+++ utils/scripts/sweep_parameters.py
@@ -57,52 +57,49 @@ if __name__ == "__main__":
         file_operations.write_input_params_from_dict(input_parameters=input_parameters,
                                                      target_filename=input_parameter_file_name_during_sweep)
         # Submit job using this parameter file
-        if not (os.path.exists("run_simulation.slurm")):
-            # Write a default run_simulation.slurm file if it does not exist in the
-            # repository
-            run_simulation_slurm = """\
-            #!/bin/bash
-            #SBATCH -J CoupledEPCondensates
-            #SBATCH --mail-user davidgoh
-            #SBATCH -p sched_mit_arupc_long
-            #SBATCH -t 12:00:00
-            #SBATCH --mem-per-cpu 4000
-            cd "$SLURM_SUBMIT_DIR"
-            echo $PWD
+        run_simulation_slurm = """\
+        #!/bin/bash
+        #SBATCH -J CoupledEPCondensates
+        #SBATCH --mail-user davidgoh
+        #SBATCH -p sched_mit_arupc_long
+        #SBATCH -t 12:00:00
+        #SBATCH --mem-per-cpu 4000
+        cd "$SLURM_SUBMIT_DIR"
+        echo $PWD
 
-            stage_parameters()
-            {
-                cp $input_file input_parameters_$SLURM_JOBID.txt
-            }
+        stage_parameters()
+        {
+            cp $input_file input_parameters_$SLURM_JOBID.txt
+        }
 
-            run_program()
-            {
-                source activate CoupledEPCondensates
-                run-simulation --i $input_file --o $out_folder
-                conda deactivate
-            }
+        run_program()
+        {
+            source activate CoupledEPCondensates
+            run-simulation --i $input_file --o $out_folder
+            conda deactivate
+        }
 
-            cleanup_files()
-            {
-                rm input_parameters_$SLURM_JOBID.txt
-            }
+        cleanup_files()
+        {
+            rm input_parameters_$SLURM_JOBID.txt
+        }
 
-            movie()
-            {
-                source activate CoupledEPCondensates
-                output_folder=$(python -c "from utils.scripts.run_simulation import get_output_dir_name as outname; from utils.file_operations import input_parse;  print(outname(input_parse('$input_file')))")
-                make-movie --i $output_folder
-                conda deactivate
-                echo "DONE"
-            }
-            stage_parameters
-            run_program
-            cleanup_files
-            movie
-            """
-            run_simulation_slurm = textwrap.dedent(run_simulation_slurm)
-            with open("run_simulation.slurm","w") as fhandle:
-                fhandle.write(run_simulation_slurm)
+        movie()
+        {
+            source activate CoupledEPCondensates
+            output_folder=$(python -c "from utils.scripts.run_simulation import get_output_dir_name as outname; from utils.file_operations import input_parse;  print(outname(input_parse('$input_file')))")
+            make-movie --i $output_folder
+            conda deactivate
+            echo "DONE"
+        }
+        stage_parameters
+        run_program
+        cleanup_files
+        movie
+        """
+        run_simulation_slurm = textwrap.dedent(run_simulation_slurm)
+        with open("run_simulation.slurm","w") as fhandle:
+            fhandle.write(run_simulation_slurm)
 
         os.system('sbatch --export=input_file={},out_folder={} run_simulation.slurm'
                     .format(input_parameter_file_name_during_sweep, output_directory))
